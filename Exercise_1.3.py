7c392d23580b20eff3c067da9419faa369ad2885
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-11-20 13:38:09 -0500

    Define take_recipe() function

--- Exercise_1.3.py
+++ Exercise_1.3.py
@@ -1,3 +1,19 @@
 recipes_list = []
 ingredients_list = []
 
+def take_recipe():
+  name = str(input('Enter the name of the recipe: '))
+  cooking_time = int(input('Enter the recipe time in minutes: '))
+  ingredients = []
+  while True:
+    ingredient = input('Enter an ingredient (or type "done" to finish): ')
+    if ingredient == 'done':
+      break
+    else:
+      ingredients.append(ingredient)
+  recipe = {
+    'name': name,
+    'cooking_time': cooking_time,
+    'ingredients': ingredients
+  }
+  return recipe # This sends the recipe dictionary back to the caller (in this case, the for loop)
