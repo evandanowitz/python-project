a6dd3421e134b4209a4277a20d929568ecec08f7
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-11-20 13:30:56 -0500

    create add.py script

--- add.py
+++ add.py
@@ -1,4 +1,14 @@
-a = int(input("Enter value of a here: "))
-b = int(input("Enter value of b here: "))
-c = a + b
-print(c)
+# a = int(input("Enter value of a here: "))
+# b = int(input("Enter value of b here: "))
+# c = a + b
+# print(c)
+
+a = input('Enter a number: ')
+b = input('Enter another number to be added to the first: ')
+a = int(a)
+b = int(b)
+print('The sum of these numbers is', a + b)
+
+# a = int(input('Enter a number: '))
+# b = int(input('Enter another number to be added to the first: '))
+# print('The sum of these numbers is ' + str(a +b))
