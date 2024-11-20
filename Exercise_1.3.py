3f0b3fff7430ee461ae2e9b6c3c0402a73952c62
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-11-20 13:39:45 -0500

    for loop to add ingredients to ingredients_list if not already present

--- Exercise_1.3.py
+++ Exercise_1.3.py
@@ -17,3 +17,16 @@ def take_recipe():
     'ingredients': ingredients
   }
   return recipe # This sends the recipe dictionary back to the caller (in this case, the for loop)
+
+n = int(input('How many recipes would you like to enter? '))
+
+for recipe in range(n): # Iterates n times for each recipe
+  recipe = take_recipe() # This assigns the output of take_recipe() to the recipe variable during each iteration
+  for ingredient in recipe['ingredients']: # Iterates over the ingredients in the current recipe
+    if ingredient not in ingredients_list: # Checks if the ingredient is already in ingredients_list
+      ingredients_list.append(ingredient) # If not already in ingredients_list, append the ingredient
+  recipes_list.append(recipe) # Add the recipe to the list of recipes
+
+# print("\nRecipes List:", recipes_list)
+# print("\nIngredients List:", ingredients_list)
+
