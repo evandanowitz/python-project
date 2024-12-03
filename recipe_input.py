2d7881b2bb9198796a3c54a644a6dec4029a2056
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-12-02 19:14:58 -0500

    Create recipe_input script

---
+++ recipe_input.py
@@ -0,0 +1,4 @@
+# This script takes recipes from the user, compiles them and their ingredients 
+# into a list, and stores all this in a binary file for later use. 
+# The script can be run again later to add more recipees
+
