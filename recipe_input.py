45dc5867e149f6244c5d13487f5d46f0b0028ab1
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-12-02 19:15:08 -0500

    Import pickle module

--- recipe_input.py
+++ recipe_input.py
@@ -2,3 +2,5 @@
 # into a list, and stores all this in a binary file for later use. 
 # The script can be run again later to add more recipees
 
+import pickle # Allows you to work with binary files later in the script
+
