4feb973645f8175f7703aba989ed23d3670f628b
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-12-02 19:12:23 -0500

    Create display_recipe() function

--- recipe_search.py
+++ recipe_search.py
@@ -2,3 +2,13 @@
 
 import pickle # Allows reading from and writing to binary files
 
+# This function should format and print the details of the recipe passed to it as an argument.
+# Displays a single recipe's details.
+def display_recipe(recipe):
+  print(f'Recipe Name: {recipe["recipe_name"]}')
+  print(f'Cooking Time: {recipe["cooking_time"]} minutes')
+  print('Ingredients:')
+  for ingredient in recipe['ingredients']:
+    print(f'{ingredient}')
+  print(f'Difficulty: {recipe["difficulty"]}')
+
