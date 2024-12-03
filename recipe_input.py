0531f2c43af4a20a6d566eda11293ac63cf426bc
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-12-02 19:15:43 -0500

    Create take_recipe() function

--- recipe_input.py
+++ recipe_input.py
@@ -4,3 +4,28 @@
 
 import pickle # Allows you to work with binary files later in the script
 
+# Function to take user input and return a recipe dictionary
+def take_recipe():
+
+  # Prompt user to enter the recipe's name
+  recipe_name = input('Enter the name of the recipe: ')
+
+  # Prompt user to enter the cooking time and convert it to an integer
+  cooking_time = int(input('Enter the cooking time (minutes): '))
+
+  # Initialize an empty list to store ingredients
+  ingredients = []
+
+  # While loop to gather ingredients until user types 'done'
+  while True:
+    # Prompt user to enter an ingredient
+    ingredient = input('Enter an ingredient (type "done" when finished): ')
+    
+    # Check if user wants to stop entering ingredients
+    if ingredient == 'done':
+      # Exit loop if 'done' is typed by user
+      break
+    else:
+      # Add the ingredient to the list
+      ingredients.append(ingredient)
+  
