865f649f5772bcdec5a121e984f76a4e21fcc8ac
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-12-09 20:40:16 -0500

    Define calculate_difficulty() method

--- Exercise-1.5/recipe_oop.py
+++ Exercise-1.5/recipe_oop.py
@@ -6,3 +6,15 @@ class Recipe(object):
     self.cooking_time = cooking_time
     self.difficulty = None # 'difficulty' attribute is auto-generated and initially set to None
 
+  # Calculates difficulty level and updated self.difficulty attribute accordingly
+  def calculate_difficulty(self):
+    num_ingredients = len(self.ingredients)
+    if self.cooking_time < 10 and num_ingredients < 4:
+      self.difficulty = 'Easy'
+    elif self.cooking_time < 10 and num_ingredients >= 4:
+      self.difficulty = 'Medium'
+    elif self.cooking_time >= 10 and num_ingredients < 4:
+      self.difficulty = 'Intermediate'
+    else:
+      self.difficulty = 'Hard'
+
