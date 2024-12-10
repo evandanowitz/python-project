db4243e665dc3a9a78f62c0c52074d4ec217a01a
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-12-09 20:39:42 -0500

    Define __init__ method for Recipe class

--- Exercise-1.5/recipe_oop.py
+++ Exercise-1.5/recipe_oop.py
@@ -1,2 +1,8 @@
 # Define the Recpe class
 class Recipe(object):
+  def __init__(self, name, ingredients, cooking_time):
+    self.name = name
+    self.ingredients = ingredients
+    self.cooking_time = cooking_time
+    self.difficulty = None # 'difficulty' attribute is auto-generated and initially set to None
+
