c089b6b2e2112b1317d4183750cb92aab6c6c548
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-12-26 17:54:58 -0500

    Initialize a database connection object

--- Exercise-1.6/recipe_mysql.py
+++ Exercise-1.6/recipe_mysql.py
@@ -1,3 +1,11 @@
 # Import the mysql.connector module
 import mysql.connector
 
+# Initialize a connection object called conn, which connects with following parameters:
+# Based on user that was set up earlier
+conn = mysql.connector.connect(
+  host = 'localhost',
+  user = 'cf-python',
+  passwd = 'password'
+)
+
