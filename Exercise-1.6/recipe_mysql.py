d3f32d36b1541571fe9853abbb8619268c092217
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-12-26 17:56:19 -0500

    Create "task_database" and database table "Recipes"

--- Exercise-1.6/recipe_mysql.py
+++ Exercise-1.6/recipe_mysql.py
@@ -12,3 +12,17 @@ conn = mysql.connector.connect(
 # Initialize a cursor object from conn
 cursor = conn.cursor()
 
+# Create database. IF NOT EXISTS ensures it is only database with its name on the server
+cursor.execute('CREATE DATABASE IF NOT EXISTS task_database')
+
+# Script to access database with the USE statement
+cursor.execute('USE task_database')
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS Recipes (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50),
+  ingredients VARCHAR(255),
+  cooking_time INT,
+  difficulty VARCHAR(20)
+)''')
+
