86d00427f4bcad1ba0d7af9b289187ce6b24b16b
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2025-01-06 14:21:03 -0500

    Add imports for SQLAlchemy toolkit

---
+++ Exercise-1.7/recipe_app.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base # changed to below
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column
+from sqlalchemy.types import Integer, String
+from sqlalchemy.orm import sessionmaker
+
