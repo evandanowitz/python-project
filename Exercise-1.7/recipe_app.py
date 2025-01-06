c58287dda62d970c8be10a8c645b6e27830aae4b
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2025-01-06 15:13:40 -0500

    Define create_recipe function

--- Exercise-1.7/recipe_app.py
+++ Exercise-1.7/recipe_app.py
@@ -59,3 +59,65 @@ class Recipe(Base): # Recipe class inherits the Base class created previously
     else:
       return self.ingredients.split(', ') # split string into list with ', '
 
+# Function 1
+def create_recipe():
+  while True: # keeps looping until the break statement is executed
+    name = input('Enter the name of the recipe (maximum of 50 characters): ')
+    if len(name) > 50: # if name is too long...
+      print('Name must be 50 characters or less. Please try again.') # print error message and let the loop run again
+    else:
+      break # exit the loop if the input is valid
+  # print(f'Recipe name accepted: {name}')
+
+  while True:
+    try:
+      cooking_time = int(input('Enter the cooking time (in minutes): '))
+      if cooking_time > 0:
+        # print(f'Recipe cooking time accepted: {cooking_time}')
+        break # exit the loop if the input is valid
+      else:
+        print('Cooking time must be a positive number. Please try again.') # retry for negative or zero
+    except Exception as e:
+      print(f'An error occurred: {e}')
+      return None
+  
+  while True:
+    try:
+      num_of_ingredients = int(input('Enter the number of ingredients to enter: '))
+      if num_of_ingredients > 0:
+        break # exit the loop if the input is valid
+      else:
+        print('Input must be a positive number. Please try again.') # retry for negative or zero
+    except Exception as e:
+      print(f'An error occurred: {e}')
+      return None
+  
+  ingredients = [] # initialize empty list to store ingredients
+  for i in range(num_of_ingredients):
+    while True: # loop to retry entering the same ingredient until valid input is provided
+      ing = input(f'Enter ingredient {i + 1}: ').strip()
+      if not ing:
+        print('Ingredient cannot be empty. Please try again.')
+      elif ing in ingredients: # prevent duplicate ingredients
+        print(f'Ingredient "{ing}" is already in the list. Please enter a different ingredient.')
+      else:
+        ingredients.append(ing) # add valid, unique ingredient
+        break # ensures program exits inner loop and movies to next ingredient only when valid input is provided
+  ingredients_str = ', '.join(ingredients) # Convert ingredients list to comma-separated string
+  # print(f'Ingredients accepted: {ingredients_str}')
+
+  recipe_entry = Recipe(
+  name = name,
+  cooking_time = cooking_time,
+  ingredients = ingredients_str
+  )
+
+  # call the calculate_difficulty() method on the recipe_entry object
+  recipe_entry.calculate_difficulty()
+
+  # add and save the recipe to the database
+  session.add(recipe_entry)
+  session.commit()
+
+  print(f'\nRecipe "{name}" has been successfully added to the database.')
+
